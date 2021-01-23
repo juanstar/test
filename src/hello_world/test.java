6c6a606a98caee27b7b28cba11ea7907840b73e0
j_sta <j_sta@LAPTOP-IBN7A47E>
2021-01-23 11:09:47 -0600

    Version 1.0

---
+++ src/hello_world/test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_world;
+
+/**
+ *
+ * @author j_sta
+ */
+public class test {
+
+        public static void main(String[] args) {
+        // TODO code application logic here
+            System.out.println("Bienvenido a clase");
+    }
+    
+}
